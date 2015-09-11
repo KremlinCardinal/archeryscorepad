ad1eda3f5c2aa248cb4b49df4acf3a8cae98db45
Michiel Dijk <support@kremlincardinal.nl>
2015-09-11 09:37:46 +0200

    - scrapped angular - made basic working of scorepad

--- index.php
+++ index.php
@@ -6,13 +6,11 @@
     <link rel="stylesheet" href="css/default.css">
 
     <script src="js/jquery-2.1.4.min.js"></script>
-    <script src="js/angular.min.js"></script>
 </head>
-<body ng-app="" ng-init="leeftijden={ 1: 'Aspirant', 2: 'Junior', 3: 'Senior' };
-                         boogtypen={ 1: 'Recurve', 2: 'Compound', 3: 'Barebow', 4: 'Traditioneel/hout' }">
+<body>
     <h1>Archery scorepad</h1>
     <hr>
-
+    <form>
         <table>
             <tr>
                 <td>
@@ -32,9 +30,9 @@
                 </td>
                 <td>
                     <select id="leeftijdsklasse" name="age">
-                        <option ng-repeat="leeftijd in leeftijden">
-                            {{ leeftijd }}
-                        </option>
+                        <option value="Aspirant">Aspirant</option>
+                        <option value="Junior">Junior</option>
+                        <option value="Senior">Senior</option>
                     </select>
                 </td>
             </tr>
@@ -42,9 +40,10 @@
                 <td><label for="boogtype">Boogtype:</label></td>
                 <td>
                     <select id="boogtype" name="bowtype">
-                        <option ng-repeat="boogtype in boogtypen">
-                            {{ boogtype }}
-                        </option>
+                        <option value="Recurve">Recurve</option>
+                        <option value="Compound">Compound</option>
+                        <option value="Barebow">Barebow</option>
+                        <option value="Traditioneel/hout">Traditioneel/hout</option>
                     </select>
                 </td>
             </tr>
@@ -64,93 +63,24 @@
             <tbody>
             <tr class="a">
                 <td>3</td>
-                <td class="a1"><input type="text" class="a1" ng-model="a1"></td>
-                <td class="a2"><input type="text" class="a2" ng-model="a2"></td>
-                <td class="a3"><input type="text" class="a3" ng-model="a3"></td>
-                <td class="a4 ser1 ser"><input type="text" class="a4" ng-model="ser1" value="{{ a1 }}"></td>
-                <td class="a5 sub1 sub"><input type="text" class="a5" ng-model="sub1" value="{{ ser1 }}"></td>
+                <td class="a1"><input type="text" class="a1" name="a1"></td>
+                <td class="a2"><input type="text" class="a2" name="a2"></td>
+                <td class="a3"><input type="text" class="a3" name="a3"></td>
+                <td class="a4 ser1 ser"><input type="text" class="a4" name="ser1"></td>
+                <td class="a5 sub1 sub"><input type="text" class="a5" name="sub1"></td>
             </tr>
-            <tr class="b">
-                <td>6</td>
-                <td class="b1"><input type="text" class="b1"></td>
-                <td class="b2"><input type="text" class="b2"></td>
-                <td class="b3"><input type="text" class="b3"></td>
-                <td class="b4 ser2 ser"><input type="text" class="b4"></td>
-                <td class="b5 sub2 sub"><input type="text" class="b5"></td>
-            </tr>
-            <tr class="c">
-                <td>9</td>
-                <td class="c1"><input type="text" class="c1"></td>
-                <td class="c2"><input type="text" class="c2"></td>
-                <td class="c3"><input type="text" class="c3"></td>
-                <td class="c4 ser3 ser"><input type="text" class="c4"></td>
-                <td class="c5 sub3 sub"><input type="text" class="c5"></td>
-            </tr>
-            <tr class="d">
-                <td>12</td>
-                <td class="d1"><input type="text" class="d1"></td>
-                <td class="d2"><input type="text" class="d2"></td>
-                <td class="d3"><input type="text" class="d3"></td>
-                <td class="d4 ser4 ser"><input type="text" class="d4"></td>
-                <td class="d5 sub4 sub"><input type="text" class="d5"></td>
-            </tr>
-            <tr class="e">
-                <td>15</td>
-                <td class="e1"><input type="text" class="e1"></td>
-                <td class="e2"><input type="text" class="e2"></td>
-                <td class="e3"><input type="text" class="e3"></td>
-                <td class="e4 ser5 ser"><input type="text" class="e4"></td>
-                <td class="e5 sub5 sub"><input type="text" class="e5"></td>
-            </tr>
-            <tr class="f">
-                <td>18</td>
-                <td class="f1"><input type="text" class="f1"></td>
-                <td class="f2"><input type="text" class="f2"></td>
-                <td class="f3"><input type="text" class="f3"></td>
-                <td class="f4 ser6 ser"><input type="text" class="f4"></td>
-                <td class="f5 sub6 sub"><input type="text" class="f5"></td>
-            </tr>
-            <tr class="g">
-                <td>21</td>
-                <td class="g1"><input type="text" class="g1"></td>
-                <td class="g2"><input type="text" class="g2"></td>
-                <td class="g3"><input type="text" class="g3"></td>
-                <td class="g4 ser7 ser"><input type="text" class="g4"></td>
-                <td class="g5 sub7 sub"><input type="text" class="g5"></td>
-            </tr>
-            <tr class="h">
-                <td>24</td>
-                <td class="h1"><input type="text" class="h1"></td>
-                <td class="h2"><input type="text" class="h2"></td>
-                <td class="h3"><input type="text" class="h3"></td>
-                <td class="h4 ser8 ser"><input type="text" class="h4"></td>
-                <td class="h5 sub8 sub"><input type="text" class="h5"></td>
-            </tr>
-            <tr class="i">
-                <td>27</td>
-                <td class="i1"><input type="text" class="i1"></td>
-                <td class="i2"><input type="text" class="i2"></td>
-                <td class="i3"><input type="text" class="i3"></td>
-                <td class="i4 ser9 ser"><input type="text" class="i4"></td>
-                <td class="i5 sub9 sub"><input type="text" class="i5"></td>
-            </tr>
-            <tr class="j">
-                <td>30</td>
-                <td class="j1"><input type="text" class="j1"></td>
-                <td class="j2"><input type="text" class="j2"></td>
-                <td class="j3"><input type="text" class="j3"></td>
-                <td class="j4 ser10 ser"><input type="text" class="j4"></td>
-                <td class="j5 sub10 sub"><input type="text" class="j5"></td>
-            </tr>
+
             <tr class="totals">
                 <td colspan="4"><label for="total">Totaal:</label></td>
                 <td class="total" colspan="2"><input id="total" type="text" class="total" name="total"></td>
             </tr>
             </tbody>
         </table>
-        <button onclick="getData()">Invoeren</button>
+    </form>
+    <button onclick="calcSubs()">Bereken Subtotaal</button>
 
-
+    <script src="js/arrays.js"></script>
     <script src="js/default.js"></script>
+
 </body>
 </html>
--- js/default.js
+++ js/default.js
@@ -1,4 +1,20 @@
 function getData() {
-    var $formdata = $('#scorepad_form').serialize();
+    var $formdata = $('form').serialize();
     console.log($formdata);
+}
+
+function calcSubs() {
+    var ser = 0;
+    var serField = 1;
+    var count = 0;
+    $.each(arrows, function(i, field) {
+        ser += ($('input[name="'+field).val())*1;
+        count++;
+        if(count >= 3) {
+            $('input[name="ser'+serField).val(ser);
+            serField++;
+            count = 0;
+            ser = 0;
+        }
+    });
 }
